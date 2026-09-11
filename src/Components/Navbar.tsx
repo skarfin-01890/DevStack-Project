8e2213221c85db61471ccd14272139555baed38e
skarfin-6568 <skarfin6568@gmail.com>
2026-09-11 22:35:34 +0600

    Navbar complete

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import LogoImg from "../assets/logo-text.png"
+
+const Navbar = () => {
+	return (
+		<div className='bg-white border border-gray-100'>
+			<div className='flex justify-between container mx-auto mt-6'>
+<div>
+	<img src={LogoImg} alt="" />
+	</div>
+
+<nav className='flex justify-center items-center gap-8 text-gray-600'>
+	<a className='text-red-500'>Home</a>
+	<a>Technologies</a>
+	<a>Projects</a>
+	<a>About</a>
+	<a>Contact</a>
+</nav>
+<div className='flex gap-4'>
+<button>Sign In</button>
+<button className='bg-[#D91B7E] text-white p-2 px-4 rounded-full'>Sign Up</button>
+</div>
+</div>
+		</div>
+	);
+};
+
+export default Navbar;
